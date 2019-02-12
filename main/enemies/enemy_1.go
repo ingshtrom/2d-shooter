1e81518c2c1f335f5f4cc1acae11a9721518e35d
Alex Hokanson <alex.hokanson@docker.com>
2019-02-11 21:01:08 -0500

    initial commit

---
+++ main/enemies/enemy_1.go
@@ -0,0 +1,35 @@
+components {
+  id: "enemy_1"
+  component: "/main/enemies/enemy_1.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/main/main.atlas\"\n"
+  "default_animation: \"red_square_black_border\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
