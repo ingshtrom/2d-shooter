97558c8c5a48f3decc279369d41b2bbe6aa11b91
Alex Hokanson <alex.hokanson@docker.com>
2019-02-12 07:55:04 -0500

    started working on bullets

--- main/weapons/weapon_1/weapon_1_bullet.go
+++ main/weapons/weapon_1/weapon_1_bullet.go
@@ -1,23 +1,8 @@
-components {
-  id: "enemy_1"
-  component: "/main/enemies/enemy_1_controller.script"
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
 embedded_components {
   id: "sprite"
   type: "sprite"
   data: "tile_set: \"/main/main.atlas\"\n"
-  "default_animation: \"red_square_black_border\"\n"
+  "default_animation: \"green_circle\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
